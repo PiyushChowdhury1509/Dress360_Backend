feafca1034e997250e2e741b104d605b6871dae8
Piyush Chowdhury <piyushchowdhury1509@gmail.com>
2025-03-15 17:21:41 +0530

    created signin api

--- src/controllers/userAuth.controller.ts
+++ src/controllers/userAuth.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express"
-import { userSchema, userTypeWithId } from "../schemas/userSchema"; 
+import { userSchema, userType, userTypeWithId } from "../schemas/userSchema"; 
 import { User } from "../models/user";
-import { hashPassword } from "../utils/password";
+import { comparePassword, hashPassword } from "../utils/password";
 import { z } from "zod";
 
 export const UserSignup = async (req: Request,res: Response) => {
@@ -52,6 +52,60 @@ export const UserSignup = async (req: Request,res: Response) => {
             message: "something went wrong with the server",
             error: err
         })
+        return;
+    }
+}
+
+export const UserSignin = async (req:Request, res: Response) => {
+    try{
+        const body = req.body;
+        const userSigninSchema = userSchema.pick({
+            email: true,
+            password: true
+        });
+        const refinedBody = userSigninSchema.parse(body);
+
+        const user: userTypeWithId | null = await User.findOne({
+            email: refinedBody.email
+        });
+
+        if(!user){
+            res.status(404).json({
+                success: false,
+                message: "no user found with this email id"
+            });
+            return;
+        }
+
+        const isPasswordCorrect = comparePassword(refinedBody.password, user.password);
+        if(!isPasswordCorrect){
+            res.status(400).json({
+                success: false,
+                message: "invalid credentials"
+            });
+            return;
+        }
+
+        const token = user.getJwt();
+        res.cookie('token',token);
+
+        res.status(200).json({
+            success: true,
+            message: "signin successfull"
+        });
+    } catch(err){
+        if(err instanceof z.ZodError){
+            res.status(400).json({
+                success: false,
+                message: "invalid request"
+            });
+            return;
+        }
+        res.status(500).json({
+            success: false,
+            message: "internal server error",
+            error: err
+        });
         return;
     }
 }
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,9 @@
 import express, { Router, RequestHandler} from 'express'
-import { UserSignup } from '../controllers/userAuth.controller'; 
+import { UserSignup, UserSignin } from '../controllers/userAuth.controller'; 
 
 const userRouter: Router = express.Router();
 
 userRouter.post('/signup', UserSignup as RequestHandler);
+userRouter.post('/signin', UserSignin as RequestHandler);
 
 export default userRouter;
